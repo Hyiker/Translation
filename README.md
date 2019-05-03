71d87069850c6b1bf031d426d76043ebde01424b
hyiker <961531608@qq.com>
2019-05-03 11:54:03 +0800

    新增API切换功能

--- README.md
+++ README.md
@@ -1,19 +1,24 @@
 # translation README
 
-VSCode 简洁翻译插件(使用有道智云)
+VSCode 简洁翻译插件
 
 ## Features
 
--   使用有道智云 API
--   显示发音、词性、词义
+-   可有多种翻译 API 可选
+-   使用有道智云 API 显示发音、词性、词义
+-   使用谷歌翻译 显示词义
 
 ## Requirements
 
 需要在[有道智云](https://ai.youdao.com/gw.s)注册并获取相应翻译 api 的 appKey(应用 ID)与秘钥
 整个过程**免费**
 
+**更新**
+默认使用谷歌翻译 无需注册等繁琐操作 但是词义、解释等较少
+
 ## Extension Settings
 
+-   `translation.API`: 使用的翻译 API 默认为谷歌`google` 可切换为有道`youdao`
 -   `translation.displayMethod`: 展示翻译的方式 `statusBar`表示在状态栏中显示，`messageWindow`表示在消息窗口中显示
 -   `translation.hideTimeout`: 使用状态栏显示时，显示翻译的持续时间(秒) (只有`statusBar`模式下方可设置)
 -   `translation.youdaoAppKey`: 有道智云应用 ID
@@ -25,10 +30,11 @@ VSCode 简洁翻译插件(使用有道智云)
 
 ## Release Notes
 
-Users appreciate release notes as you update your extension.
-
 ### 0.0.1
 
 凑合着用吧...
 
+### 0.1.0
+
+新增谷歌翻译 API 并默认使用
 **Enjoy!**
--- extension.js
+++ extension.js
@@ -24,62 +24,13 @@ function activate(context) {
             const editor = vscode.window.activeTextEditor;
             const selection = editor.selection;
             const text = editor.document.getText(selection);
-            var input = text;
-            if (text.length > 20) {
-                input = text.substr(0, 10) + text.length + text.substr(-10, 10);
+            const api = configuration.get("translation.API");
+            console.log(text);
+            if (api == "youdao") {
+                getMeaningFromYoudao(text);
+            } else {
+                getMeaningFromGoogle(text);
             }
-            var ctime = Math.round(new Date().getTime() / 1000) + "";
-            var body = {
-                q: text,
-                from: "auto",
-                to: "auto",
-                appKey: configuration.get("translation.youdaoAppKey"),
-                curtime: ctime,
-                salt: uuid
-                    .v1()
-                    .toString()
-                    .replace(/-/g, ""),
-                signType: "v3"
-            };
-            body["sign"] = sha256(
-                body["appKey"] +
-                    input +
-                    body["salt"] +
-                    ctime +
-                    configuration.get("translation.youdaoSecretKey")
-            );
-
-            axios.default
-                .get("http://openapi.youdao.com/api", { params: body })
-                .then(function(response) {
-                    console.log(response.data);
-                    const basic = response.data.basic;
-
-                    var info = "";
-                    if (basic && basic["phonetic"]) {
-                        info += "[" + basic["phonetic"] + "]; ";
-                    }
-                    if (
-                        basic &&
-                        basic["explains"] &&
-                        basic["explains"].length > 0
-                    ) {
-                        basic["explains"].forEach(element => {
-                            info += element + "; ";
-                        });
-                    }
-                    if (
-                        configuration.get("translation.displayMethod") ===
-                        "statusBar"
-                    ) {
-                        vscode.window.setStatusBarMessage(
-                            info,
-                            configuration.get("translation.hideTimeout") * 1000
-                        );
-                    } else {
-                        vscode.window.showInformationMessage(info);
-                    }
-                });
         }
     );
 
@@ -87,6 +38,97 @@ function activate(context) {
 }
 exports.activate = activate;
 
+function getMeaningFromYoudao(text) {
+    var input = text;
+    if (text.length > 20) {
+        input = text.substr(0, 10) + text.length + text.substr(-10, 10);
+    }
+    var ctime = Math.round(new Date().getTime() / 1000) + "";
+    var body = {
+        q: text,
+        from: "auto",
+        to: "auto",
+        appKey: configuration.get("translation.youdaoAppKey"),
+        curtime: ctime,
+        salt: uuid
+            .v1()
+            .toString()
+            .replace(/-/g, ""),
+        signType: "v3"
+    };
+    body["sign"] = sha256(
+        body["appKey"] +
+            input +
+            body["salt"] +
+            ctime +
+            configuration.get("translation.youdaoSecretKey")
+    );
+
+    axios.default
+        .get("http://openapi.youdao.com/api", { params: body })
+        .then(function(response) {
+            if (response.data.errorCode != "0") {
+                vscode.window.showErrorMessage(
+                    "请求出现错误,请检查appKey/secretKey是否正确"
+                );
+                return;
+            }
+            const basic = response.data.basic;
+            var info = "";
+            if (basic && basic["phonetic"]) {
+                info += "[" + basic["phonetic"] + "]; ";
+            }
+            if (basic && basic["explains"] && basic["explains"].length > 0) {
+                basic["explains"].forEach(element => {
+                    info += element + "; ";
+                });
+            }
+            displayMeaning(info);
+        });
+}
+
+function getMeaningFromGoogle(text) {
+    var from = "",
+        to = "";
+    if (/[\u4E00-\u9FA5\uF900-\uFA2D]/.test(text)) {
+        from = "zh";
+        to = "en";
+    } else {
+        from = "en";
+        to = "zh";
+    }
+    axios.default
+        .get(
+            encodeURI(
+                `http://translate.google.cn/translate_a/single?client=gtx&dt=t&dj=1&ie=UTF-8&sl=${from}&tl=${to}&q=${text}`
+            )
+        )
+        .then(function(response) {
+            if (response.status != 200) {
+                vscode.window.showErrorMessage("谷歌API请求出现问题");
+                return;
+            }
+            var translations = response.data.sentences;
+            if (translations && translations.length > 0) {
+                var info = text + ": ";
+                translations.forEach(element => {
+                    info += element.trans + "; ";
+                });
+                displayMeaning(info);
+            }
+        });
+}
+
+function displayMeaning(info) {
+    if (configuration.get("translation.displayMethod") === "statusBar") {
+        vscode.window.setStatusBarMessage(
+            info,
+            configuration.get("translation.hideTimeout") * 1000
+        );
+    } else {
+        vscode.window.showInformationMessage(info);
+    }
+}
 // this method is called when your extension is deactivated
 function deactivate() {}
 
